ccfb362ae2f6be11820825dcc3284af1f5e81bf4
Joel Field <joelmatthewfield@outlook.com>
2018-05-11 07:36:54 +1000

    bring in react router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {HashRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <Route path="/" component={App}/>
+    </HashRouter>,
+    document.getElementById('root')
+);
 registerServiceWorker();
